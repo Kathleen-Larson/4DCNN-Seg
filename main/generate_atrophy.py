9e4324656e5922e90aacac9a3ebbe3e8000e5692
Kathleen-Larson <klarson9@mgh.harvard.edu>
2025-04-04 11:05:33 -0400

    fixed bug in lesion insertion

--- main/generate_atrophy.py
+++ main/generate_atrophy.py
@@ -11,31 +11,37 @@ import torch.optim
 import torch.nn as nn
 import torch.nn.functional as F
 
-from utils import fatal
+import utils
 
 #------------------------------------------------------------------------------
 
 class SynthLongitudinal(nn.Module):
     def __init__(self,
-                 sdict:dict,
-                 IOdict:dict,
-                 in_shape:tuple[int],          # no. expected shape of input
-                 bg_labels:list[int]=0,        # background label val
-                 include_lsns:bool=False,      # flag to do lesion insertion
-                 lsns_in:list[int]=[2, 41],    # labels to insert lesions
-                 lsns_label:int=77,            # lesion label val
-                 max_n_lsns:int=0,             # max no. lesions to insert
-                 n_dims:int=3,                 # no. image dimesions (2 or 3)
-                 n_tps:int=2,                  # no. timepoints to synthesize
-                 subsample_atrophy:float=1.0,  # % to subsamle atrophy
-                 T:int=2,                      # index of temporal dim.
+                 sdict:dict,                  # dict of labels to atrophy
+                 IOdict:dict,                 # dict of in->out label mappings
+                 in_shape:list[int],           # no. expected shape of input
+                 bg_labels:list[int]=0,       # background label val
+                 do_insert_lsns:bool=False,   # flag to insert lesions
+                 do_resize_labels:bool=True,  # flag to do synthetic atrophy
+                 lsns_in_label:list[int]=2,   # labels to insert lesions
+                 lsns_label:int=77,           # lesion label val
+                 max_n_lsns:int=0,            # max no. lesions to insert
+                 n_dims:int=3,                # no. image dimesions (2 or 3)
+                 n_tps:int=2,                 # no. timepoints to synthesize
+                 subsample:float=1.0,         # % to subsamle atrophy
+                 T:int=2,                     # index of temporal dim.
                  device=None,
     ):
         super(SynthLongitudinal, self).__init__()
-        self.synth_atrophy = nn.Module()
+        
+        # Parse class attributes
         self.device = 'cpu' if device is None else device
         self.n_timepoints = n_tps
+        self.n_dims = n_dims
 
+        max_n_lsns = max_n_lsns if do_insert_lsns else 0
+        sdict = sdict if do_resize_labels else None
+        
         # Check inputs
         in_shape = ((int(in_shape),) * n_dims
                     if isinstance(in_shape, int) or isinstance(in_shape, float)
@@ -68,7 +74,7 @@ class SynthLongitudinal(nn.Module):
             model.add_module('ResizeLabels', resize_labels)
             model.add_module('LabelsToImage', labels_to_image)
             self.synth_atrophy.add_module(f'SynthModel{t}', model)
-
+            
 
     def forward(self, y_in):
         X = [None] * self.n_timepoints
@@ -76,25 +82,31 @@ class SynthLongitudinal(nn.Module):
 
         for t in range(self.n_timepoints):
             model = self.synth_atrophy.__getattr__(f'SynthModel{t}')
-            breakpoint()
             y[t] = model.AddLesions(y_in if t == 0 else y[t-1].clone())
             y[t] = model.ResizeLabels(y[t])
-            X[t] = synth_model.LabelsToImage(y[t])
-            
-        return torch.stack(X, dim=self.T), torch.stack(y, dim=self.T)
+            X[t] = model.LabelsToImage(y[t])
+
+        X = torch.stack(X, dim=-(self.n_dims+1))
+        y = torch.stack(y, dim=-(self.n_dims+1))
+        return X, y
 
     
 #-----------------------------------------------------------------------------
 
 class _AddLesions(nn.Module):
+    """
+    Class to add a random number of lesions (up to max_n_lesions) within a 
+    label image. Lesions will have a maximum volume of max_lesions_vol.
+    """
     def __init__(self, 
                  in_shape=None,
-                 lesions_label:int=77,          # value for lesion label
-                 chance:float=0.5,           # prob. of inserting lesions
-                 max_n_lesions:int=1,           # max no. of lesions
-                 bg_labels:list[int]=[2,41], # labels to insert lesions
-                 bg_buffer:int=3,
-                 max_lesions_vol:float=20.0,
+                 lesions_label:int=77,        # value for lesion label
+                 chance:float=0.5,            # prob. of inserting lesions
+                 max_n_lesions:int=1,         # max no. of lesions
+                 bg_labels:list[int]=[2,41],  # labels to insert lesions
+                 bg_buffer:int=3,             # size of buffer (no. voxels)
+                 max_lesions_vol:float=20.0,  # max lesion volume
+                 n_dims:int=3,                # no. image dims
                  device=None
     ):
         super(_AddLesions, self).__init__()
@@ -106,8 +118,10 @@ class _AddLesions(nn.Module):
         self.bg_buffer = bg_buffer
         self.bg_labels = bg_labels
         self.max_lesions_vol = max_lesions_vol
+        self.n_dims = n_dims
 
-        assert in_shape is not None, "Must provide input shape to _AddLesions"
+        if in_shape is None:
+            utils.fatal('Must provide input shape to _AddLesions')
         
         # Set up morphology operations
         structuring_element = torch.zeros((3, 3, 3), dtype=float)
@@ -125,82 +139,76 @@ class _AddLesions(nn.Module):
 
 
     def _insert_lesions(self, x):
-        # Create eroded labels masks
-        n_bg_labels = len(self.bg_labels)
-        M_bg = [None] * n_bg_labels
-        M_bg_idxs = [None] * n_bg_labels
-
-        t_start = time.time()
-        for n in range(n_bg_labels):
-            M_bg[n] = self._make_label_mask(x, self.bg_labels[n]).float()
-            M_bg_idxs[n] = torch.where(M_bg[n] > 0.)
-            
-        # Lesion insertion
+        """
+        Insert lesions within a volume
+        """
+        # Get mask of background labels
+        M = torch.zeros(x.shape, device=x.device).to(float)
+        for bg in self.bg_labels:
+            M[x==bg] = 1.
+        bg_idxs = torch.stack(torch.where(M > 0.))
+        
+        # Initialize up to max_n_lesions lesions (1 vox in size)        
         n_lesions = random.randint(1, self.max_n_lesions)
-        rand_idxs = torch.randint(n_bg_labels, size=(n_lesions,)).tolist()
-        lesion_vols = (self.max_lesions_vol * torch.rand(size=(n_lesions,))
-        ).tolist()
-        M_les = torch.zeros(x.shape, device=x.device, dtype=x.dtype)
-        breakpoint()
-        for n, idx in enumerate(rand_idxs):
-            t0 = time.time()
-            x_les = M_bg[idx].clone()
-            lesions_sdict = {2: [lesion_vols[n], 1]}
-            
-            # Introduce 1 vox sized lesion
-            vox_idx = random.randint(0, int(M_bg[idx].sum())-1)
-            idx = [idxs[vox_idx].item() for idxs in M_bg_idxs[idx]]
-            x_les[..., idx[-3], idx[-2], idx[-1]] = 2
-
-            # Dilate lesion and update
-            grow_lesions = _ResizeLabels(lesions_sdict, x.shape,
-                                         apply_dropout=True)
-            x_les = grow_lesions(x_les)
-            M_les[x_les==2] = 1.
-            
-        x[torch.where(M_les > 0.)] = self.lesions_label
+        M[tuple(bg_idxs[:,torch.randint(0, int(M.sum()), size=(n_lesions,))]
+        )] = torch.arange(2, n_lesions+2).to(float).to(M.device)
+        
+        # Create sdict and grow lesions w/ _ResizeLabels
+        lesions_sdict = {}
+        for n in range(n_lesions):
+            lesions_sdict[n+2] = [random.uniform(0., self.max_lesions_vol), 1]
+        M = _ResizeLabels(
+            lesions_sdict, x.shape, device=self.device,
+            apply_dropout=True, randomize=False,
+        )(M)
+        
+        # Insert back into original volume
+        x = torch.where(M > 1, self.lesions_label, x)
         return x
 
     
-    def _make_label_mask(self, x, labels=None):
-        bg_labels = self.bg_labels if labels is None else labels
-        mask = torch.zeros(x.shape, device=x.device, dtype=x.dtype)
-        for n in self.bg_labels:
-            mask[x==n] = 1.
-        return mask
-    
-
     def forward(self, x):
+        """
+        Calls function w/ probability of self.chance
+        """
         if random.uniform(0., 1.) <= self.chance:
             x = self._insert_lesions(x)
         return x
     
     
+#-----------------------------------------------------------------------------
 
 class _ResizeLabels(nn.Module):
+    """
+    Class to randomly resize labels (synthetic atrophy/growth) based in an 
+    input dictionary of structures self.sdict. Has the ability to randomize 
+    the amount of atrophy and number of structures w/in the input dict on the 
+    fly.
+    """
     def __init__(self,
-                 sdict:dict={},              # dict of structures to atrophy
-                 in_shape=None,               # dims of input volume
-                 max_perc_atrophy:float=0.5,  # max perc atrophy 
-                 subsample:float=1.0,         # amt to subsample image
-                 apply_dropout=False,         # flag to do dropout (dil. masks)
-                 dropout_rate:float=0.2,      # dropout rate for dil. masks
+                 sdict:dict={},           # dict of structures to atrophy
+                 in_shape=None,           # dims of input volume
+                 subsample:float=1.0,     # amt to subsample image
+                 apply_dropout=False,     # flag to do dropout (dil. masks)
+                 dropout_rate:float=0.2,  # dropout rate for dil. masks
+                 randomize:bool=True,     # flag to randomize
                  device=None,
     ):
         super(_ResizeLabels, self).__init__()
         self.device = 'cpu' if device is None else device
 
-        self.structure_list = sdict
+        # Parse input params
+        self.sdict = sdict
         self.subsample = subsample
         self.n_channels = in_shape[1]
         self.dropout_rate = dropout_rate if apply_dropout else 0.
+        self.randomize = randomize
         
-        # Set up dilation
+        # Set up dilation convolutions
         structuring_element = torch.zeros((3, 3, 3), dtype=float)
         structuring_element[torch.tensor([1,1,2,0,1,1,1]),
                             torch.tensor([1,1,1,1,1,0,2]),
-                            torch.tensor([1,2,1,1,0,1,1])] = 1.
-        
+                            torch.tensor([1,2,1,1,0,1,1])] = 1.        
         self.dilation_conv = init_convolution(
             in_shape=in_shape, out_shape=in_shape,
             conv_weight_data=structuring_element,
@@ -209,8 +217,11 @@ class _ResizeLabels(nn.Module):
             requires_grad=False
         )
 
-
     def _apply_dropout_to_mask(self, x, dr):
+        """
+        Applies a dropout filter to an input mask (adds variability to the 
+        label boundaries during dilation/erosion)
+        """
         m = torch.zeros(x.shape, dtype=x.dtype, device=x.device)
         m[x.nonzero(as_tuple=True)] = torch.rand(
             (int(x.sum())), dtype=x.dtype, device=x.device
@@ -219,6 +230,9 @@ class _ResizeLabels(nn.Module):
         
         
     def _is_adjacent(self, x, label1, label2):
+        """
+        Determines whether or not two labels in the same volume are adjacent
+        """
         mask1 = dilate_binary_mask(
             (x == label1).float(), self.dilation_conv)
         mask2 = dilate_binary_mask(
@@ -227,6 +241,10 @@ class _ResizeLabels(nn.Module):
 
 
     def _shift_label_boundary(self, mask1, mask2, max_vol_change:float=None):
+        """
+        Moves the boundary between two labels to induce a specific amount of 
+        volumetric change (mask1 will grow, mask2 will shrink)
+        """
         dil = dilate_binary_mask(mask1, self.dilation_conv) * mask2
 
         # Apply dropout if necessary
@@ -244,6 +262,9 @@ class _ResizeLabels(nn.Module):
     
 
     def _resize_labels(self, x, sdict):
+        """
+        Main function to resize the labels specified in sdict
+        """
         it = 0
         for label in sdict:
             # Create mask of specified adjacent neighbor labels
@@ -281,27 +302,33 @@ class _ResizeLabels(nn.Module):
                         
                 vol_change = ((trg_mask.sum() - trg_mask_orig.sum()) \
                               / trg_mask_orig.sum()).item()
-
+                
         return x
 
 
-    def _randomize_atrophy(self):
-        n_atrophy = random.randint(2, len(self.structure_list))
-        idxs_all = torch.arange(0, len(self.structure_list))
+    def _randomize_sdict(self):
+        """
+        Randomize the no. structures and maximum amount of atrophy included in
+        the sdict (allows for randomization beyond class initialization)
+        """
+        # Get idxs of structures to atrophy
+        n_atrophy = random.randint(0, len(self.sdict))
+        idxs_all = torch.arange(0, len(self.sdict))
         idxs = torch.multinomial(idxs_all.to(torch.float), n_atrophy)
 
+        # Randomize each amount to be atrophied
         sdict = {}
-        for n, label in enumerate(self.structure_list.keys()):
-            max_perc = self.structure_list[label][0]
+        for n, label in enumerate(self.sdict.keys()):
+            max_perc = self.sdict[label][0]
             perc = random.uniform(max_perc, 0.) if max_perc < 0 \
                 else random.uniform(0., max_perc)
-            sdict[label] = [perc] + self.structure_list[label][1:]        
+            sdict[label] = [perc] + self.sdict[label][1:]        
 
         return sdict
 
 
     def forward(self, x):
-        sdict = self._randomize_atrophy()
+        sdict = self._randomize_sdict() if self.randomize else self.sdict
         x = self._resize_labels(x, sdict)
         return x
 
@@ -316,13 +343,14 @@ class _LabelsToImage(nn.Module):
                  in_shape:list[int],
                  bg_labels:list[int]=None,
                  max_intensity:int=255,
-                 blur_std:float=0.5,
+                 blur_std:float=0.2,
                  noise_std=0.15,
                  device=None
     ):
         super(_LabelsToImage, self).__init__()
         self.device = 'cpu' if device is None else device
-        
+
+        # Parse args
         self.IOdict = IOdict
         self.bg_labels = [bg_labels] if not isinstance(bg_labels, list) \
             else bg_labels
@@ -341,6 +369,10 @@ class _LabelsToImage(nn.Module):
 
         
     def _convert_labels(self, x):
+        """
+        Given the dictionary self.IOdict, converts voxels equal to a key to
+        its corresponding value
+        """
         y = x.clone()
         for label in self.IOdict:
             if self.IOdict[label] != label:
@@ -349,30 +381,29 @@ class _LabelsToImage(nn.Module):
 
 
     def _add_noise(self, x):
+        """
+        Add Gaussian white noise to input image
+        """
         sigma = random.uniform(0., self.noise_std)
         noise = torch.normal(mean=0., std=sigma, size=tuple(x.shape),
                              dtype=x.dtype, device=x.device)
         return x + noise
 
 
-    def _min_max_norm(self, x, minim=0., maxim=255.):
-        return (maxim - minim) * (x - x.min()) / (x.max() - x.min()) + minim
-    
-    
-
     def _synthesize_intensities(self, x):
+        """
+        Create synthetic grayscale image from input label map
+        """
+        # Get list of foreground labels and intensity means
+        labels = list(set(x.unique().tolist()) ^ set(self.bg_labels))
+        means = torch.rand(len(labels)) * self.max_intensity
+
+        # Fill y
         y = torch.zeros(x.shape, dtype=torch.float, device=x.device)
-        labels = x.unique().tolist()
-        for idx, val in enumerate(labels):
-            if val in self.bg_labels:  labels.pop(idx)
-
-        means = torch.rand(len(labels)).to(y.dtype)
-        for idx, label in enumerate(labels):
-            inds = torch.where(x==label)
-            y[inds] = means[idx]
-
+        for label, m in zip(labels, means):
+            y[torch.where(x==label)] = m.item()
+            
         return y
-
 
         
     def forward(self, y):
@@ -380,7 +411,7 @@ class _LabelsToImage(nn.Module):
         X = self._synthesize_intensities(X)
         X = self._add_noise(X)
         X = self.gaussian_blur(X)
-        X = self._min_max_norm(X)
+        X = utils._min_max_norm(X, m=0, M=255)
         return X
 
     
@@ -392,17 +423,17 @@ def _config_synth_models(config:dict, include_lesions=True, device=None):
     """    
     # Load label lookup table
     if not 'lut' in config:
-        fatal('Error: must include path to lut in input config')
+        utils.fatal('Error: must include path to lut in input config')
     lut = sf.load_label_lookup(config['lut'])
 
-    # Get dict of corresponding left/right label values (d = {left: right})
+    # Get dict of corresponding right/left label values (d = {right: left})
     lr_dict = {}
-    for Lkey, Lval in lut.items():
-        if 'Left' in Lval.name:
-            lr_dict[Lkey] = search_lut(
-                lut, '-'.join(['Right'] + Lval.name.split('-')[1:])
+    for Rkey, Rval in lut.items():
+        if 'Right' in Rval.name:
+            lr_dict[Rkey] = search_lut(
+                lut, '-'.join(['Left'] + Rval.name.split('-')[1:])
             )[0]
-
+    
     # Get label dictionaries
     synth_slists_config = yaml.safe_load(open(config['slist_config']))
     synth_slists = {}
@@ -419,7 +450,8 @@ def _config_synth_models(config:dict, include_lesions=True, device=None):
     synth_models = {}
     for _class, sdict in synth_slists.items():
         synth_models[_class] = SynthLongitudinal(
-            sdict=sdict, IOdict=lr_dict, **config['model_config']
+            sdict=sdict, IOdict=lr_dict, device=device,
+            **config['model_config']
         )
 
     return synth_models
@@ -460,10 +492,10 @@ def gaussian_kernel(sigma:float, ndims:int=3):
 
 
 def init_convolution(in_shape, out_shape, conv_weight_data,
-                          kernel_size:int=3, stride:int=1,
-                          padding:int=1, dilation:int=1,
-                          bias=False, ndims:int=3, device=None,
-                          requires_grad:bool=False,
+                     kernel_size:int=3, stride:int=1,
+                     padding:int=1, dilation:int=1,
+                     bias=False, ndims:int=3, device=None,
+                     requires_grad:bool=False,
 ):
     in_channels = in_shape[1]
     out_channels = out_shape[1]
@@ -479,7 +511,6 @@ def init_convolution(in_shape, out_shape, conv_weight_data,
         )
     conv_fn.weight.data = conv_weight_data.to(torch.float).to(device)
     conv_fn.weight.requires_grad = requires_grad
-
     return conv_fn
 
 
